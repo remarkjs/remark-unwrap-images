0e4aa2cdabc5ff29739894e90cbafb6212326696
Titus Wormer <tituswormer@gmail.com>
2019-06-18 11:00:20 +0200

    Rewrite to support white-space, multiple images
    This rewrites the algorithm to support multiple nodes (links, images), white
    space between them.
    This disallows non-image and non-link content, such as emphasis, because with
    the previous behaviour the following would also unwrap:
    
    ```markdown
    **a strong node with ![an image](image.png) and other stuff.**
    ```
    
    Closes GH-1.
    Closes GH-4.

--- index.js
+++ index.js
@@ -1,17 +1,61 @@
-const visit = require('unist-util-visit-parents')
+var visit = require('unist-util-visit')
+var whiteSpace = require('hast-util-whitespace')
 
-module.exports = () => tree =>
-  visit(tree, ['image', 'imageReference'], (node, ancestors) => {
-    const parent = ancestors
-      .slice()
-      .reverse()
-      .find(ancestor => ancestor.type === 'paragraph')
+module.exports = unwrapImages
 
-    if (parent.children.length === 1) {
-      const firstChild = parent.children[0]
+var unknown = null
+var containsImage = true
+var containsOther = false
 
-      for (const key in firstChild) {
-        parent[key] = firstChild[key]
+var splice = [].splice
+
+function unwrapImages() {
+  return transform
+}
+
+function transform(tree) {
+  visit(tree, 'paragraph', onparagraph)
+}
+
+function onparagraph(node, index, parent) {
+  if (applicable(node) === containsImage) {
+    splice.apply(parent.children, [index, 1].concat(node.children))
+    return [visit.SKIP, index]
+  }
+}
+
+function applicable(node, inLink) {
+  var image = unknown
+  var children = node.children
+  var length = children.length
+  var index = -1
+  var child
+  var linkResult
+
+  while (++index < length) {
+    child = children[index]
+
+    if (whiteSpace(child)) {
+      // White space is fine.
+    } else if (child.type === 'image' || child.type === 'imageReference') {
+      image = containsImage
+    } else if (
+      !inLink &&
+      (child.type === 'link' || child.type === 'linkReference')
+    ) {
+      linkResult = applicable(child, true)
+
+      if (linkResult === containsOther) {
+        return containsOther
       }
+
+      if (linkResult === containsImage) {
+        image = containsImage
+      }
+    } else {
+      return containsOther
     }
-  })
+  }
+
+  return image
+}
--- test.js
+++ test.js
@@ -18,6 +18,16 @@ test('remark-unwrap-images', function(t) {
     remark()
       .use(unwrap)
       .use(html)
+      .processSync('![alpha](alpha.png) ![bravo](bravo.png)')
+      .toString(),
+    '<img src="alpha.png" alt="alpha">\n \n<img src="bravo.png" alt="bravo">\n',
+    'should unwrap multiple images'
+  )
+
+  t.equal(
+    remark()
+      .use(unwrap)
+      .use(html)
       .processSync('some text ![and](and.png) an image')
       .toString(),
     '<p>some text <img src="and.png" alt="and"> an image</p>\n',
@@ -28,10 +38,50 @@ test('remark-unwrap-images', function(t) {
     remark()
       .use(unwrap)
       .use(html)
+      .processSync('some text')
+      .toString(),
+    '<p>some text</p>\n',
+    'should not unwrap if there are no images'
+  )
+
+  t.equal(
+    remark()
+      .use(unwrap)
+      .use(html)
+      .processSync('[](#remark)')
+      .toString(),
+    '<p><a href="#remark"></a></p>\n',
+    'should not unwrap if there are no images in links'
+  )
+
+  t.equal(
+    remark()
+      .use(unwrap)
+      .use(html)
       .processSync('[![hi](there.png)](#remark)')
       .toString(),
     '<a href="#remark"><img src="there.png" alt="hi"></a>\n',
     'should supports links'
+  )
+
+  t.equal(
+    remark()
+      .use(unwrap)
+      .use(html)
+      .processSync('[![hi](there.png)](#remark)!')
+      .toString(),
+    '<p><a href="#remark"><img src="there.png" alt="hi"></a>!</p>\n',
+    'should not unwrap links next to other content'
+  )
+
+  t.equal(
+    remark()
+      .use(unwrap)
+      .use(html)
+      .processSync('[![Hello](there.png), world](#remark)')
+      .toString(),
+    '<p><a href="#remark"><img src="there.png" alt="Hello">, world</a></p>\n',
+    'should not unwrap links with other content'
   )
 
   t.equal(
