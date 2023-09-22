4c2769e43c8921ffd6e4dacf12c7e1a8c57fc295
Titus Wormer <tituswormer@gmail.com>
2023-09-22 18:03:36 +0200

    Refactor to use `node:test`

--- test.js
+++ test.js
@@ -1,98 +1,124 @@
-import test from 'tape'
+import assert from 'node:assert/strict'
+import test from 'node:test'
 import {remark} from 'remark'
 import remarkHtml from 'remark-html'
 import remarkUnwrapImages from './index.js'
 
-test('remarkUnwrapImages', (t) => {
-  t.equal(
-    remark()
-      .use(remarkUnwrapImages)
-      .use(remarkHtml)
-      .processSync('![hi](there.png)')
-      .toString(),
-    '<img src="there.png" alt="hi">\n',
-    'should unwrap images'
+test('remarkUnwrapImages', async function (t) {
+  await t.test('should unwrap images', async function () {
+    assert.equal(
+      String(
+        await remark()
+          .use(remarkUnwrapImages)
+          .use(remarkHtml)
+          .process('![hi](there.png)')
+      ),
+      '<img src="there.png" alt="hi">\n'
+    )
+  })
+
+  await t.test('should unwrap multiple images', async function () {
+    assert.equal(
+      String(
+        await remark()
+          .use(remarkUnwrapImages)
+          .use(remarkHtml)
+          .process('![alpha](alpha.png) ![bravo](bravo.png)')
+      ),
+      '<img src="alpha.png" alt="alpha">\n \n<img src="bravo.png" alt="bravo">\n'
+    )
+  })
+
+  await t.test(
+    'should not unwrap images next to other content',
+    async function () {
+      assert.equal(
+        String(
+          await remark()
+            .use(remarkUnwrapImages)
+            .use(remarkHtml)
+            .process('some text ![and](and.png) an image')
+        ),
+        '<p>some text <img src="and.png" alt="and"> an image</p>\n'
+      )
+    }
   )
 
-  t.equal(
-    remark()
-      .use(remarkUnwrapImages)
-      .use(remarkHtml)
-      .processSync('![alpha](alpha.png) ![bravo](bravo.png)')
-      .toString(),
-    '<img src="alpha.png" alt="alpha">\n \n<img src="bravo.png" alt="bravo">\n',
-    'should unwrap multiple images'
+  await t.test('should not unwrap if there are no images', async function () {
+    assert.equal(
+      String(
+        await remark()
+          .use(remarkUnwrapImages)
+          .use(remarkHtml)
+          .process('some text')
+      ),
+      '<p>some text</p>\n'
+    )
+  })
+
+  await t.test(
+    'should not unwrap if there are no images in links',
+    async function () {
+      assert.equal(
+        String(
+          await remark()
+            .use(remarkUnwrapImages)
+            .use(remarkHtml)
+            .process('[](#remark)')
+        ),
+        '<p><a href="#remark"></a></p>\n'
+      )
+    }
   )
 
-  t.equal(
-    remark()
-      .use(remarkUnwrapImages)
-      .use(remarkHtml)
-      .processSync('some text ![and](and.png) an image')
-      .toString(),
-    '<p>some text <img src="and.png" alt="and"> an image</p>\n',
-    'should not unwrap images next to other content'
+  await t.test('should supports links', async function () {
+    assert.equal(
+      String(
+        await remark()
+          .use(remarkUnwrapImages)
+          .use(remarkHtml)
+          .process('[![hi](there.png)](#remark)')
+      ),
+      '<a href="#remark"><img src="there.png" alt="hi"></a>\n'
+    )
+  })
+
+  await t.test(
+    'should not unwrap links next to other content',
+    async function () {
+      assert.equal(
+        String(
+          await remark()
+            .use(remarkUnwrapImages)
+            .use(remarkHtml)
+            .process('[![hi](there.png)](#remark)!')
+        ),
+        '<p><a href="#remark"><img src="there.png" alt="hi"></a>!</p>\n'
+      )
+    }
   )
 
-  t.equal(
-    remark()
-      .use(remarkUnwrapImages)
-      .use(remarkHtml)
-      .processSync('some text')
-      .toString(),
-    '<p>some text</p>\n',
-    'should not unwrap if there are no images'
-  )
+  await t.test('should not unwrap links with other content', async function () {
+    assert.equal(
+      String(
+        await remark()
+          .use(remarkUnwrapImages)
+          .use(remarkHtml)
+          .process('[![Hello](there.png), world](#remark)')
+      ),
+      '<p><a href="#remark"><img src="there.png" alt="Hello">, world</a></p>\n'
+    )
+  })
 
-  t.equal(
-    remark()
-      .use(remarkUnwrapImages)
-      .use(remarkHtml)
-      .processSync('[](#remark)')
-      .toString(),
-    '<p><a href="#remark"></a></p>\n',
-    'should not unwrap if there are no images in links'
-  )
-
-  t.equal(
-    remark()
-      .use(remarkUnwrapImages)
-      .use(remarkHtml)
-      .processSync('[![hi](there.png)](#remark)')
-      .toString(),
-    '<a href="#remark"><img src="there.png" alt="hi"></a>\n',
-    'should supports links'
-  )
-
-  t.equal(
-    remark()
-      .use(remarkUnwrapImages)
-      .use(remarkHtml)
-      .processSync('[![hi](there.png)](#remark)!')
-      .toString(),
-    '<p><a href="#remark"><img src="there.png" alt="hi"></a>!</p>\n',
-    'should not unwrap links next to other content'
-  )
-
-  t.equal(
-    remark()
-      .use(remarkUnwrapImages)
-      .use(remarkHtml)
-      .processSync('[![Hello](there.png), world](#remark)')
-      .toString(),
-    '<p><a href="#remark"><img src="there.png" alt="Hello">, world</a></p>\n',
-    'should not unwrap links with other content'
-  )
-
-  t.equal(
-    remark()
-      .use(remarkUnwrapImages)
-      .use(remarkHtml)
-      .processSync('![hi][image]\n\n[image]: kitten.png')
-      .toString(),
-    '<img src="kitten.png" alt="hi">\n',
-    'should supports image references'
-  )
-
-  t.end()
+  await t.test('should supports image references', async function () {
+    assert.equal(
+      String(
+        await remark()
+          .use(remarkUnwrapImages)
+          .use(remarkHtml)
+          .process('![hi][image]\n\n[image]: kitten.png')
+      ),
+      '<img src="kitten.png" alt="hi">\n'
+    )
+  })
 })
